c0c762da2575ce20ca8fa9dbf40f1f4542f56d67
michaelwiderin <michael.widerin@hotmail.com>
2022-06-16 19:49:46 +0200

    created native android project

--- src/app/app/hooks/useSocket.js
+++
@@ -1,7 +0,0 @@
-import { SocketContext } from "../store/socket-context";
-import { useContext } from "react";
-
-export const useSocket = () => {
-    const socket = useContext(SocketContext);
-    return socket;
-}
